efd48fa4156f91aff586d0c2d49c1867c9f32107
BarniBl <VeinskiyVA@mail.ru>
2020-03-03 00:37:29 +0300

    fixed directory request bug

--- src/response.py
+++ src/response.py
@@ -14,7 +14,7 @@ content_types = {
     'txt': 'text/plain'
 }
 
-status_types = {
+status_codes = {
     '200': 'OK',
     '403': 'Forbidden',
     '404': 'Not Found',
@@ -24,47 +24,37 @@ status_types = {
 GET = 'GET'
 HEAD = 'HEAD'
 INFO = 'HTTP/1.1'
-index = 'index.html'
+INDEX = 'index.html'
 
 
 class HttpResponse:
     def __init__(self, request, document_root):
-        self.headers = {
-            'Content-Type:': '',
-            'Content-Length:': '',
-            'Date:': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            'Server:': 'server/1.0',
-            'Connection:': 'close'
-        }
         self.request = request
         self.request_path = ''
-        self.file_path = ''
         self.document_root = document_root
 
-    def add_header(self, type, body):
-        self.headers.update({type: body})
-
     def response_with_error(self, code):
-        response = INFO + ' {} {}\r\n'.format(code, status_types[str(code)])
-        response += '{} {}\r\n'.format('Date:', str(self.headers['Date:']))
-        response += '{} {}\r\n'.format('Server:', str(self.headers['Server:']))
-        response += '{} {}\r\n'.format('Connection:', str(self.headers['Connection:']))
+        response = INFO + ' {} {}\r\n'.format(code, status_codes[str(code)])
+        response += '{} {}\r\n'.format('Date:', str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        response += '{} {}\r\n'.format('Server:', 'server/1.0')
+        response += '{} {}\r\n'.format('Connection:', 'closed')
         response += '\n'
         return response.encode()
 
-    def response_get_head(self, param=''):
+    def response_message(self, method='', file_path=''):
         try:
-            file = open(self.file_path, 'rb')
+            file = open(file_path, 'rb')
             body = file.read()
             file.close()
 
-            self.add_header('Content-Length:', str(len(body)))
+            response = 'HTTP/1.1 {} {}\r\n'.format(200, status_codes['200'])
+            response += '{} {}\r\n'.format('Content-Type:', content_types[file_path.split('.')[-1]])
+            response += '{} {}\r\n'.format('Content-Length:', os.path.getsize(file_path))
+            response += '{} {}\r\n'.format('Date:', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            response += '{} {}\r\n'.format('Server:', 'server/1.0')
+            response += '{} {}\r\n'.format('Connection:', 'closed')
 
-            response = 'HTTP/1.1 {} {}\r\n'.format(200, status_types['200'])
-            for key in self.headers:
-                response += '{} {}\r\n'.format(key, str(self.headers[key]))
-
-            if param == GET:
+            if method == GET:
                 response += '\n'
                 return response.encode() + body
 
@@ -73,30 +63,30 @@ class HttpResponse:
         except IOError:
             return self.response_with_error(404)
 
-    def create_response(self):
+    def get_request_path(self):
         request_first_line = self.request.split('\r\n')[0].split(' ')
 
-        request_method = request_first_line[0]
-        if not (request_method == GET or request_method == HEAD):
+        if request_first_line[0] not in [GET, HEAD]:
             return self.response_with_error(405)
 
-        self.request_path = parse.urlparse(parse.unquote(request_first_line[1])).path
+        self.request_path = parse.unquote(request_first_line[1])
+
+        return request_first_line[0]
+
+    def create_response(self):
+        method = self.get_request_path()
 
         if self.request_path.find('../') != -1:
             return self.response_with_error(404)
 
-        if os.path.isdir(self.request_path):
-            self.file_path = self.document_root + self.request_path + index
-            if not (os.path.isfile(self.file_path)):
+        if self.request_path[len(self.request_path) - 1] == '/':
+            file_path = self.document_root + self.request_path + INDEX
+            if not (os.path.isfile(file_path)):
                 return self.response_with_error(403)
         else:
-            self.file_path = self.document_root + self.request_path
+            file_path = self.document_root + self.request_path
 
-        if not os.path.isfile(self.file_path):
-            return self.response_with_error(404)
-        self.add_header('Content-Type:', content_types[self.file_path.split('.')[-1]])
+        if method == GET:
+            return self.response_message(GET, file_path)
 
-        if request_method == GET:
-            return self.response_get_head(GET)
-
-        return self.response_get_head()
+        return self.response_message(HEAD, file_path)
